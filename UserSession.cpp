031ac2541373b7eaea86532f15b97d07615051a6
Elisei <47348743+ezenevich@users.noreply.github.com>
2025-07-01 19:21:24 +0300

    Revert "Fix user session initialization"

--- UserSession.cpp
+++
@@ -1,6 +0,0 @@
-#include "UserSession.h"
-
-String^ UserSession::_userFIO = nullptr;
-String^ UserSession::_username = nullptr;
-String^ UserSession::_role = nullptr;
-String^ UserSession::_currentClass = nullptr;
